acfc2a037b10ded505f334661bcc39d6ff20007a
Natalia Georgina Lo Duca <natiloduca@gmail.com>
2019-04-30 08:38:36 -0300

    ngrx angular store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlockchainComponent } from './blockchain/blockchain.component';
+import { UserComponent } from './user/user.component';
+import { StoreModule } from '@ngrx/store';
+import { addCoinReducer } from './reducers/blockchain.reducer';
+import { addUserReducer } from './reducers/user.reducer';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DisplayComponent } from './display/display.component';
+// import { reducers, metaReducers, reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
+/*     StoreModule.forRoot(reducers, { metaReducers }),
+!environment.production ? StoreDevtoolsModule.instrument() : []*/
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlockchainComponent,
+    UserComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ blockchain: addCoinReducer, user: addUserReducer }),
+    ReactiveFormsModule,
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
